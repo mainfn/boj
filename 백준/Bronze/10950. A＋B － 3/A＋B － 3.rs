73393bdbd368460c1a3650b18a46987d8d55435c
mainfn <121966058+mainfn@users.noreply.github.com>
2023-09-09 23:47:55 +0900

    [Bronze V] Title: A+B - 3, Time: 8 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.rs
@@ -0,0 +1,33 @@
+use std::{
+    io::{stdin, Read},
+    str::FromStr,
+};
+
+fn main() {
+    truncate_line();
+    let nums = input_vec::<i32>();
+
+    for i in (0..nums.len()).step_by(2) {
+        let sum = nums[i] + nums[i + 1];
+        println!("{sum}");
+    }
+}
+
+fn truncate_line() {
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+}
+
+fn input_vec<T>() -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    buf.trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<T>>()
+}
