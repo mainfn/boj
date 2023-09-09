08c2cea6b6504d1a2c2c3c269a31dd868dbbef23
mainfn <121966058+mainfn@users.noreply.github.com>
2023-09-09 19:21:31 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 8 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.rs
@@ -0,0 +1,22 @@
+use std::io::stdin;
+use std::str::FromStr;
+
+fn main() {
+    let n = input_number::<usize>();
+
+    for i in 1..=n {
+        let spaces = " ".repeat(n - i);
+        let stars = "*".repeat(i);
+        println!("{spaces}{stars}");
+    }
+}
+
+fn input_number<T>() -> T
+where
+    T: Eq + Ord + Copy + FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<T>().unwrap()
+}
