e6e3e555697752986340b2f61c7b7356190c5419
mainfn <121966058+mainfn@users.noreply.github.com>
2023-09-09 23:49:06 +0900

    [Bronze V] Title: A+B - 5, Time: 8 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.rs
@@ -0,0 +1,27 @@
+use std::{
+    io::{stdin, Read},
+    str::FromStr,
+};
+
+fn main() {
+    let nums = input_vec::<i32>();
+
+    for i in (0..nums.len() - 2).step_by(2) {
+        let sum = nums[i] + nums[i + 1];
+        println!("{sum}");
+    }
+}
+
+fn input_vec<T>() -> Vec<T>
+where
+    T: FromStr,
+    <T as FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+
+    buf.trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<T>>()
+}
