db4af7a28e88e0006cc89f0d098276a55501539c
mainfn <121966058+mainfn@users.noreply.github.com>
2023-09-24 13:14:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 16440 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.rs
@@ -0,0 +1,47 @@
+use std::fmt::Write;
+use std::io::stdin;
+use std::str::FromStr;
+
+fn main() {
+    let mut n = input_number::<isize>();
+
+    let mut coords = Vec::<(i32, i32)>::new();
+
+    for _ in 0..n {
+        let nums = input_line_as::<i32>();
+        coords.push((nums[0], nums[1]));
+    }
+
+    coords.sort();
+
+    let mut output = String::new();
+    for coord in coords {
+        writeln!(&mut output, "{} {}", coord.0, coord.1).unwrap();
+    }
+
+    print!("{output}");
+}
+
+fn input_number<T>() -> T
+    where
+        T: Eq + Ord + Copy + FromStr,
+        <T as FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+    buf.trim().parse::<T>().unwrap()
+}
+
+fn input_line_as<T>() -> Vec<T>
+    where
+        T: Eq + Ord + Copy + FromStr,
+        <T as FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).unwrap();
+
+    buf.trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<T>>()
+}
